a906b0afc24812531d7bcd23ddb07383546e56fd
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 11:26:20 +0100

    Return proper Table in client/src/App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { read } from './utils/api';
 import AddChannelForm from './components/AddChannelForm';
+import Table from './components/Table';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ApiData } from "./utils/api.d";
@@ -20,6 +21,9 @@ export default function App() {
     return (
         <Container className="App">
             <h1>{JSON.stringify(data)}</h1>
+            <Row>
+                <Col><Table categories={data ? data.columns : false} rows={data ? data.channels.data : false}/></Col>
+            </Row>
             <Row>
                 <Col><AddChannelForm columns_properties={data ? data.columns_properties : false} /></Col>
             </Row>
