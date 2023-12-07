17d7fbf9b8c56482ac11e7a0e91fbb88e2b4d706
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 10:11:01 +0100

    Create client table.util unit tests

---
+++ client/src/utils/__tests__/table.util.test.ts
@@ -0,0 +1,22 @@
+import { errorHandler } from "../table.util";
+
+test("error handler should return false if no errors", () => {
+    const data = {
+        name: { value: "test", limit: 10 },
+        clients: { value: 5, limit: 10 }
+    }
+    const errors = errorHandler(data);
+    expect(errors).toBe(false);
+});
+
+test("error handler should return object with errors", () => {
+    const data = {
+        name: { value: "test", limit: 2 },
+        clients: { value: 15, limit: 10 }
+    }
+    const errors = errorHandler(data);
+    expect(errors).toHaveProperty("name");
+    expect(errors).toHaveProperty("clients");
+});
+
+export {} // this file needs to be a module
