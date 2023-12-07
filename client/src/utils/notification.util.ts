4cde170ee2630b1dae6d0cae7fcc5c5e74d0c6e8
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 10:42:30 +0100

    Add new codes to notification.util.ts

--- client/src/utils/notification.util.ts
+++ client/src/utils/notification.util.ts
@@ -8,9 +8,12 @@ export const generate = (code: number, message: string) => {
         body: 'Nieoczekiwany błąd. Skontaktuj się z administratorem.'
     }
 
-    if (code === 1) notification.body = 'Ponowna próba połączenia z serwerem...';
-    if (code === 500) notification.body = 'Nie można połączyć się z serwerem. Spróbuj ponownie później lub skontaktuj się z administratorem. Ponowna próba połączenia nastąpi za 10 sekund.';
-    if (code === 200) notification.body = 'Połączenie z serwerem zostało nawiązane.';
+    if (code === 0) notification.body = 'Nie można połączyć się z serwerem. Następna próba nastąpi za 5 sekund.';
+    if (code === 1) notification.body = 'Pobieranie danych z serwera...';
+    if (code === 2) notification.body = 'Próba połączenia z serwerem...';
+    if (code === 3) notification.body = 'Udało się połączyć z serwerem.';
+    if (code === 500) notification.body = 'Nie można pobrać danych z serwera. Następna próba nastąpi za 10 sekund.';
+    if (code === 200) notification.body = 'Pobrano dane z serwera.';
     if (code === 409) notification.body = "Kanał z podaną nazwą już istnieje."
 
     if (code==1000 || code===1001) {
