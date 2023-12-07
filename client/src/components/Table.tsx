80075f6c7c363db34d204d9a2cf1adb28a18cf44
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 11:26:05 +0100

    Change way of generating elements in Table

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -2,18 +2,17 @@ import { Table as BootstrapTable } from 'react-bootstrap';
 
 const Table = (props: any) => {
 
-    const categories = props.categories.map((category: any) => {
-        return <th>{category}</th>
-    })
+    const categories = props.categories ? props.categories.map((category: any, index: number) => {
+        return <th key={index}>{category}</th>
+    }) : null;
 
-    const rows = props.rows.map((row: any) => {
-        const items = row.values().map((item: any) => {
-            return <td>{item}</td>
+    const rows = props.rows ? props.rows.map((row: any, row_index: number) => {
+        const items = Object.values(row).map((item: any, item_index: number) => {
+            return <td key={item_index}>{item}</td>
         })
 
-        return (<tr>{items}</tr>)
-    })
-
+        return (<tr key={row_index}>{items}</tr>)
+    }) : null;
 
     return (
         <BootstrapTable striped bordered hover>
