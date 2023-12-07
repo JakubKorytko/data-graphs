6c48c9b18468a000d59e0ce68610e2d04d47a749
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 11:29:25 +0100

    Fill client/src/App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react';
+import { read } from './utils/api';
+import AddChannelForm from './components/AddChannelForm';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ApiData } from "./utils/api.d";
+
 export default function App() {
+
+    const [data, setData] = useState<ApiData|false>(false);
+
+    const fetchData = () => {
+        read().then((res) => {
+            setData(res);
+        })
+    }
+
+    useEffect(fetchData, []);
+
     return (
-        <div className="App">
-            <h1>Hello</h1>
-        </div>
+        <Container className="App">
+            <h1>{JSON.stringify(data)}</h1>
+            <Row>
+                <Col><AddChannelForm columns_properties={data ? data.columns_properties : false} /></Col>
+            </Row>
+        </Container>
     )
 }
