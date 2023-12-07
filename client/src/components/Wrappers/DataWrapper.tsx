32799668944c739b5a859e3ce7395e3101ce9c35
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 10:34:49 +0100

    Return promise in DataWrapper

--- client/src/components/Wrappers/DataWrapper.tsx
+++ client/src/components/Wrappers/DataWrapper.tsx
@@ -1,7 +1,7 @@
 import { DataWrapperProps } from "../../types/props";
-import { ApiData, CustomError } from "../../types/api";
+import { ApiData, CustomMessage } from "../../types/api";
 import { useState, useEffect, useContext } from "react";
-import { read,} from "../../utils/api.util";
+import { read, } from "../../utils/api.util";
 import chartColors from "../../utils/chartColors.util";
 import { DataContext, NotificationsContext } from "./Context";
 
@@ -9,30 +9,37 @@ import { DataContext, NotificationsContext } from "./Context";
 const DataWrapper = (props: DataWrapperProps) => {
 
     const [data, setData] = useState<ApiData | false>(false);
-    const {notify} = useContext(NotificationsContext);
-    
+    const { notify } = useContext(NotificationsContext);
+
     let isFetching = false;
     let delayedTimeouts: number[] = [];
 
     const fetchData = () => {
         if (isFetching) return;
-        if (!props.connection) return; 
+        if (!props.connection) return;
 
         isFetching = true;
-        read().then((res) => {
-            if (res.type == 'error') reconnectLoop(res as CustomError)
-            else {
-                const data = res as ApiData;
-                setData({...data, chartColors: chartColors(data.channels.data.length)});
-                
-                notify({ code: 200, message: "Data fetched from the server" }, 5);
+        return new Promise((resolve, reject) => {
+            read().then((res) => {
+                if (res.type == 'error') {
+                    reconnectLoop(res as CustomMessage)
+                    reject(res);
+                }
+                else {
+                    const data = res as ApiData;
+                    setData({ ...data, chartColors: chartColors(data.channels.data.length) });
+                    
+                    notify({ code: 200, message: "Data fetched from the server" }, 5);
 
-                isFetching = false;
-            }
+                    isFetching = false;
+
+                    resolve(res);
+                }
+            })
         });
     }
 
-    const reconnectLoop = (error: CustomError) => {
+    const reconnectLoop = (error: CustomMessage) => {
 
         notify(error, 10);
 
@@ -58,7 +65,7 @@ const DataWrapper = (props: DataWrapperProps) => {
     }
 
     return <DataContext.Provider value={contextProxy}>{props.children}</DataContext.Provider>
-    
+
 }
 
 export default DataWrapper;
