f3822d7c54071a4903ea231a17376c661c335cff
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 11:33:47 +0100

    Rename & fill acquisition_channels table migration

--- server/database/migrations/2023_04_08_140054_create_acquisition_channels_table.php
+++ server/database/migrations/2023_04_08_140054_create_acquisition_channels_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('acquisition_channels', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("name");
+            $table->integer("amount");
             $table->timestamps();
+            $table->unique("name");
         });
     }
 
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('acquisition_channels');
     }
 };
