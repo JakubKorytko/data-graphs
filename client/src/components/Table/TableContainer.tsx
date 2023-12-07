a85d4a2814e79151d3b1e23d081d4a7965b6749d
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 10:47:47 +0100

    Use context in client TableContainer component

--- client/src/components/Table/TableContainer.tsx
+++ client/src/components/Table/TableContainer.tsx
@@ -1,8 +1,16 @@
 import { Row, Col } from 'react-bootstrap';
 import Table from './Table';
 import TableForm from './Form/Container';
+import { useContext } from 'react';
+import { DataContext } from '../Wrappers/Context';
 
 const TableContainer = () => {
+
+    const { data } = useContext(DataContext);
+
+    const isFetched = data !== false;
+    
+    if (!isFetched) return null;
 
     return (
         <Row>
