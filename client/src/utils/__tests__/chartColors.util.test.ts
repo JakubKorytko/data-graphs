5fbbabdf217873c7e54a8e8dbd8eee3de1d34dd3
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 10:10:20 +0100

    Create client chartColors.util unit tests

---
+++ client/src/utils/__tests__/chartColors.util.test.ts
@@ -0,0 +1,11 @@
+import chartColors from "../chartColors.util"
+
+test("chart colors should be proper rgba strings", () => {
+    const colors = chartColors(5);
+    expect(colors).toHaveLength(5);
+    colors.forEach((color: string) => {
+        expect(color).toMatch(/^rgba\((\d{1,3}), (\d{1,3}), (\d{1,3}), 1\)$/);
+    })
+})
+
+export {} // this file needs to be a module
