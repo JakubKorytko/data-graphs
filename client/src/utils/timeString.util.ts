165d6d745fda2d36c6cedcac9ece893e77eb90ce
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 10:54:02 +0100

    Add timeString client utility

---
+++ client/src/utils/timeString.util.ts
@@ -0,0 +1,15 @@
+const timeToString = (timeInMs: number): string => {
+    if (timeInMs < 1000) return 'less than a second ago';
+    if (timeInMs < 60000) return `${Math.floor(timeInMs / 1000)} seconds ago`;
+    if (timeInMs < 3600000) return `${Math.floor(timeInMs / 60000)} minutes ago`;
+    if (timeInMs < 86400000) return `${Math.floor(timeInMs / 3600000)} hours ago`;
+    return 'more than a day ago';
+}
+
+const generateTime = (launchedTime: number) => {
+    const diff = Date.now() - launchedTime;
+    const timeString = timeToString(diff);
+    return timeString;
+}
+
+export default generateTime;
