637ba975ee1f5acebbcc2c2a3b87a5470da64ad3
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 11:07:26 +0100

    Add new type & fix spacing in utils/api.d.ts

--- client/src/utils/api.d.ts
+++ client/src/utils/api.d.ts
@@ -1,28 +1,30 @@
 export interface ApiData {
-    columns:            string[];
+    type: string;
+    columns: string[];
     columns_properties: ColumnsProperties;
-    channels:           Channels;
+    channels: Channels;
+    chartColors: string[];
 }
 
 export interface Channels {
-    current_page:   number;
-    data:           any[];
+    current_page: number;
+    data: any[];
     first_page_url: string;
-    from:           null;
-    last_page:      number;
-    last_page_url:  string;
-    links:          Link[];
-    next_page_url:  null;
-    path:           string;
-    per_page:       number;
-    prev_page_url:  null;
-    to:             null;
-    total:          number;
+    from: null;
+    last_page: number;
+    last_page_url: string;
+    links: Link[];
+    next_page_url: null;
+    path: string;
+    per_page: number;
+    prev_page_url: null;
+    to: null;
+    total: number;
 }
 
 export interface Link {
-    url:    null | string;
-    label:  string;
+    url: null | string;
+    label: string;
     active: boolean;
 }
 
@@ -33,4 +35,10 @@ export interface ColumnsProperties {
 export interface Property {
     type: string;
     size: number;
+}
+
+export interface CustomError {
+    type: string,
+    code: number,
+    message: string
 }
