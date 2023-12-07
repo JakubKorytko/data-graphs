355f6818d2fa654ff0e30212de894f72855e5a27
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 10:25:42 +0100

    Add server routes unit tests

---
+++ server/tests/Unit/RoutesTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Http\Controllers\AcquisitionChannelController;
+
+class RoutesTest extends TestCase
+{
+
+    public static string $parameter = "?test=true";
+
+    public function test_healthcheck_route(): void
+    {
+        $response = $this->get('/status');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_data_read_route(): void {
+
+        $response = $this->get("/channels/read");
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'columns',
+            'columns_properties',
+            'channels'
+        ]);
+        $response->assertJsonFragment(
+            [
+                'columns' => [
+                    'id',
+                    'name',
+                    'clients'
+                ],
+                'columns_properties' => [
+                    'name' => [
+                        'type' => 'string',
+                        'size' => AcquisitionChannelController::$name_size
+                    ],
+                    'clients' => [
+                        'type' => 'number',
+                        'size' => AcquisitionChannelController::$clients_size
+                    ]
+                ]
+            ]
+        );
+    }
+
+    public function test_data_create_route(): void {
+
+        $try = -1;
+        $max_try = 1000;
+        $res = true;
+
+        while($try<$max_try && $res) {
+            $try++;
+            $res = AcquisitionChannelController::find("name", "test".$try);
+        }
+
+        $response = $this->post("/channels/create".RoutesTest::$parameter, [
+            'name' => 'test'.$try,
+            'clients' => 0
+        ]);
+
+        if ($try === $max_try)
+            $response->assertStatus(400);
+        else
+            $response->assertStatus(201);
+    }
+
+    public function test_data_update_route(): void {
+            
+            $try = -1;
+            $max_try = 1000;
+            $res = false;
+    
+            while($try<$max_try && !$res) {
+                $try++;
+                $res = AcquisitionChannelController::find("id", $try);
+            }
+
+            $url = "/channels/update/".($try === $max_try ? -1 : $try).RoutesTest::$parameter;
+    
+            $response = $this->put($url, [
+                'name' => 'test',
+                'clients' => 0
+            ]);
+    
+            if ($try === $max_try)
+                $response->assertStatus(404);
+            else
+                $response->assertStatus(200);
+    }
+
+    public function test_data_delete_route(): void {
+                
+            $try = -1;
+            $max_try = 1000;
+            $res = false;
+    
+            while($try<$max_try && !$res) {
+                $try++;
+                $res = AcquisitionChannelController::find("id", $try);
+            }
+    
+            $url = "/channels/delete/".($try === $max_try ? -1 : $try).RoutesTest::$parameter;
+
+            $response = $this->delete($url);
+    
+            if ($try === $max_try)
+                $response->assertStatus(404);
+            else
+                $response->assertStatus(204);
+    }
+}
