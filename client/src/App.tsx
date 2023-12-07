1d30139c88dcf9336d7818f0607a15ebfb36df04
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 10:48:23 +0100

    Use Loader instead of DataWrapper in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,9 @@ import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Chart from './components/Chart/Chart';
 import TableContainer from './components/Table/TableContainer';
-import DataWrapper from './components/Wrappers/DataWrapper';
 import NotificationsWrapper from './components/Wrappers/NotificationsWrapper';
 import style from './styles/App.style';
+import Loader from './components/Wrappers/Loader';
 
 export default function App() {
 
@@ -12,17 +12,17 @@ export default function App() {
         <Container className="App">
             <div style={style.background}></div>
             <NotificationsWrapper>
-                <DataWrapper>
-                    <div style={style.div}>
-                        <h1>Statystyki kanałów</h1>
-                    </div>
-                    <Row style={style.row}>
-                        <Col style={style.col}>
-                            <Chart/>
-                        </Col>
-                    </Row>
-                    <TableContainer />
-                </DataWrapper>
+                <Loader>
+                        <div style={style.div}>
+                            <h1>Statystyki kanałów</h1>
+                        </div>
+                        <Row style={style.row}>
+                            <Col style={style.col}>
+                                <Chart />
+                            </Col>
+                        </Row>
+                        <TableContainer />
+                </Loader>
             </NotificationsWrapper>
         </Container>
     )
