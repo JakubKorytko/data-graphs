08cbdb66ddac0c15e719ba03bb0849f8dfe4da45
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 10:47:31 +0100

    Correct formatting in Chart.config.ts

--- client/src/components/Chart/Chart.config.ts
+++ client/src/components/Chart/Chart.config.ts
@@ -1,34 +1,35 @@
 export const options = {
-    plugins: {
-      datalabels: {
-        color: "#FFF", font: {
-          size: 15,
-        },
-        formatter: (value: number, context: any) => {
-          if (value === 0) return '';
-          return value;
+  plugins: {
+    datalabels: {
+      color: "#FFF", font: {
+        size: 15,
       },
-    textStrokeColor: 'black',
-    textStrokeWidth: 2,
-    textShadowColor: 'black',
-    textShadowBlur: 2,}
+      formatter: (value: number, context: any) => {
+       if (value === 0) return '';
+      },
+      textStrokeColor: 'black',
+      textStrokeWidth: 2,
+      textShadowColor: 'black',
+      textShadowBlur: 2,
+      display: "auto"
     }
   }
+}
 
 export const data = (data: false | any[], colors: string[]) => {
 
-    const config = {
-      labels: data ? data.map(item => item.name) : [],
-      datasets: [
-        {
-          label: 'Number of clients',
-          data: data ? data.map(item => item.clients) : [],
-          backgroundColor: colors,
-          borderColor: "black",
-          borderWidth: 1,
-        },
-      ],
-    };
+  const config = {
+    labels: data ? data.map(item => item.name) : [],
+    datasets: [
+      {
+        label: 'Number of clients',
+        data: data ? data.map(item => item.clients) : [],
+        backgroundColor: colors,
+        borderColor: "black",
+        borderWidth: 1,
+      },
+    ],
+  };
 
-    return config;
+  return config;
 }
