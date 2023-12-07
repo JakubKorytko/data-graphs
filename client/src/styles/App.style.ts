7605fae839453b2bbb9ea9d5e770c66edaddcf80
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 10:54:41 +0100

    Add client/src/styles/App.style.ts

---
+++ client/src/styles/App.style.ts
@@ -0,0 +1,40 @@
+const center = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+}
+
+const style = {
+    col: {
+        ...center, 
+        paddingTop: "10px", 
+        width: "350px", 
+        height: "350px",
+        marginBottom: "80px"
+    },
+
+    div: {
+        ...center, 
+        marginBottom: "15px" 
+    },
+
+    row: {
+        justifyContent: "center"
+    },
+
+    background: {
+        position: "fixed" as "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        zIndex: -1,
+        backgroundColor: "#f5f4f4",
+        opacity: 1,
+        backgroundImage:  "radial-gradient(#000000 0.35000000000000003px, transparent 0.35000000000000003px), radial-gradient(#000000 0.35000000000000003px, #f5f4f4 0.35000000000000003px)",
+        backgroundSize: "14px 14px",
+        backgroundPosition: "0 0,7px 7px"
+    }
+}
+
+export default style;
