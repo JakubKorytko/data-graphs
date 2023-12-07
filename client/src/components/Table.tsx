6a9a160670d726931ee7a943dcc2859506d03826
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 11:06:57 +0100

    Extend client Table component

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,4 +1,9 @@
-import { Table as BootstrapTable } from 'react-bootstrap';
+import { Table as BootstrapTable, Button } from 'react-bootstrap';
+
+const centerContent = {
+    textAlign: "center" as "center",
+    verticalAlign: "middle",
+}
 
 const Table = (props: any) => {
 
@@ -6,16 +11,47 @@ const Table = (props: any) => {
         return <th key={index}>{category}</th>
     }) : null;
 
+
+    const sum = props.rows ? props.rows.reduce((acc: any, row: any) => {
+        return acc + row[props.categories[props.categories.length - 1]];
+    }, 0) : 0;
+
+    console.log(sum);
+
     const rows = props.rows ? props.rows.map((row: any, row_index: number) => {
+
         const items = Object.values(row).map((item: any, item_index: number) => {
-            return <td key={item_index}>{item}</td>
+            return <td style={centerContent} key={item_index}>{item}</td>
         })
 
-        return (<tr key={row_index}>{items}</tr>)
+        const buttons = [{ variant: "light", text: "Usuń", colspan: 1 }, { variant: "light", text: "Edytuj", colspan: 1 }]
+        // const buttons = [{variant: "primary", text: "Edytuj"}, {variant: "success", text: "Zapisz"}, {variant: "danger", text: "Usuń"}]
+        const percent = (row[props.categories[props.categories.length - 1]] / sum * 100).toFixed(2);
+        const color = props.colors[row_index];
+
+        const percents = <td style={{ ...centerContent, width: "10%" }}>
+            <div style={
+                {
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: color,
+                    color: "white",
+                    textAlign: "center",
+                    verticalAlign: "middle"
+                }}>
+                {percent}%
+            </div>
+        </td>
+
+        const buttonsList = buttons.map((button, index) => {
+            return <td key={index} colSpan={button.colspan} style={{ ...centerContent, width: "10%" }}><Button key={index} variant={button.variant}>{button.text}</Button></td>
+        })
+
+        return (<tr key={row_index}>{items}{percents}{buttonsList}</tr>)
     }) : null;
 
     return (
-        <BootstrapTable striped bordered hover>
+        <BootstrapTable striped bordered hover style={{ backgroundColor: "white" }}>
             <thead>
                 <tr>
                     {categories}
@@ -23,6 +59,7 @@ const Table = (props: any) => {
             </thead>
             <tbody>
                 {rows}
+                {props.children}
             </tbody>
         </BootstrapTable>
     );
